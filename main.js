86347731aa7bd80c55d2b3d0a028bf80e6101a4c
NehalJain1 <90461957+NehalJain1@users.noreply.github.com>
2022-07-11 15:44:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+timer_count = 0;
+timer_check = "";
+Sketch_guess = "";
+answer = "";
+score = "";
+array_1=["pen","tooth","tree","apple","banana","bag","cake","drum","chair","donut","shirt","spectacles","flower","smiley","bird","clock","eye","tent","wheel","ice-cream","binoculars","telephone","foot","hand","television","lion","panda","octopus","campfire","helicopter"];
+random_no = Math.floor((Math.random()*array_1.length)+1);
+Element_of_array = array_1[random_no];
+console.log(Element_of_array);
+document.getElementById("Sketch2draw").innerHTML = "Sketch to be drawn: " + Element_of_array;
